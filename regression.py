4f6ca9028f0c60bd2102a1ab39b611e8f07e28ec
Stanislav Ermokhin <ermohin1999@gmail.com>
2020-07-17 18:45:33 +0700

    updated regression.py: new filenames, distinct function for data selection

--- regression.py
+++ regression.py
@@ -16,6 +16,21 @@ from statsmodels.tsa.arima_model import ARIMA
 from datetime import datetime
 
 
+def filter_data(data):
+    """
+
+    :param data: dict
+    :return: pandas.DataFrame
+    """
+
+    df = pd.DataFrame(data, columns=['multi_index', 'value'])
+    year = df['multi_index'][0][1]
+    df['year'] = [int(year) + i for i in range(len(df.index))]
+    df = df.drop(columns=['multi_index'], axis=1)
+
+    return df
+
+
 def linear_model(name_data, ts_data_dep, ts_data_indep):
     """
 
@@ -26,7 +41,7 @@ def linear_model(name_data, ts_data_dep, ts_data_indep):
     """
 
     now = str(datetime.now().timestamp())
-    filename = 'models/LINEAR_model_' + name_data + now[:now.find('.')] + '.txt'
+    filename = 'models/LINEAR_model_' + '_'.join(name_data.split()) + '_' + now[:now.find('.')] + '.txt'
 
     ts_data_indep = sm.add_constant(ts_data_indep)
     reg = sm.OLS(ts_data_dep, ts_data_indep)
@@ -36,15 +51,11 @@ def linear_model(name_data, ts_data_dep, ts_data_indep):
     with open(filename, 'w') as a:
         a.write(str(results))
 
-    df = pd.DataFrame(ts_data_dep.items(), columns=['multi_index', 'value'])
-    year = df['multi_index'][0][1]
-    df['year'] = [int(year) + i for i in range(len(df.index))]
-    df = df.drop(columns=['multi_index'], axis=1)
-    dic = df.to_dict('index')
+    dic = filter_data(ts_data_dep.items()).to_dict('index')
     nd = {dic[key]['year']: float(dic[key]['value'])
           for key in dic}
 
-    json_filename = 'models/LINEAR_model_' + name_data + now[:now.find('.')] + '.json'
+    json_filename = 'models/LINEAR_model_' + '_'.join(name_data.split()) + '_' + now[:now.find('.')] + '.json'
     with open(json_filename, 'w') as a:
         json.dump(nd, a)
 
@@ -74,12 +85,10 @@ def arima_model(name_data, ts_data, p=None, d=None, q=None, prdct=None):
         model_data = model_fit._results.data.__dict__
 
         ddic = model_data['orig_endog'].items()
-        df = pd.DataFrame(ddic, columns=['multi_index', 'value'])
-        year = df['multi_index'][0][1]
-        df['year'] = [int(year) + i for i in range(len(df.index))]
-        df = df.drop(columns=['multi_index'], axis=1)
-        dic = df.to_dict('index')
-        year_since = int(df['year'].tail(1))
+
+        filtered_data = filter_data(ddic)
+        dic = filtered_data.to_dict('index')
+        year_since = int(filtered_data['year'].tail(1))
         forec = model_fit.forecast(steps=prdct)
         prediction = forec[0]
         nd = {dic[key]['year']: float(dic[key]['value'])
@@ -89,12 +98,12 @@ def arima_model(name_data, ts_data, p=None, d=None, q=None, prdct=None):
                        for i in range(1, len(prediction)+1)}
         results = model_fit.summary()
         now = str(datetime.now().timestamp())
-        filename = 'models/ARIMA_model_' + name_data + now[:now.find('.')] + '.txt'
+        filename = 'models/ARIMA_model_' + '_'.join(name_data.split()) + '_' + now[:now.find('.')] + '.txt'
 
         with open(filename, 'w') as a:
             a.write(str(results))
 
-        json_filename = 'models/ARIMA_model_' + name_data + now[:now.find('.')] + '.json'
+        json_filename = 'models/ARIMA_model_' + '_'.join(name_data.split()) + '_' + now[:now.find('.')] + '.json'
         nd.update(predictions)
         with open(json_filename, 'w') as a:
             json.dump(nd, a)
