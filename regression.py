f8489d0e717c2ba02ac084fa8d0b5ca4958f19f3
Stanislav Ermokhin <ermohin1999@gmail.com>
2020-07-21 16:41:03 +0700

    updated regression.py: new function for stationarity testing (not supported in main)

--- regression.py
+++ regression.py
@@ -6,14 +6,40 @@ Developer: Stanislav Alexandrovich Ermokhin
 
 """
 
-
 import statsmodels.api as sm
 # import pmdarima as pm
 import pandas as pd
 import json
 
+from statsmodels.tsa.stattools import adfuller as adf
+from statsmodels.tsa.stattools import kpss
+from arch.unitroot import VarianceRatio as vr, ADF as adf, KPSS as kpss
 from statsmodels.tsa.arima_model import ARIMA
 from datetime import datetime
+from numpy.random import randint
+
+
+def get_stationary_i(ts_data):
+    """
+
+    :param ts_data: list of lists
+    :return: list (len = len(ts_data))
+    """
+
+    integrated_as = [[0, 0, 0]
+                     for _ in range(len(ts_data))]
+
+    for i in range(len(ts_data)):
+        integrated_as[i][0] = adf(ts_data[i]).lags
+        integrated_as[i][1] = vr(ts_data[i]).lags
+        integrated_as[i][2] = kpss(ts_data[i]).lags
+
+    for i in range(len(integrated_as)):
+        ints = {integrated_as[i].count(integrated_as[i][j]): integrated_as[i][j]
+                for j in (0, 1, 2)}
+        integrated_as[i] = ints[max(ints)]
+
+    return integrated_as
 
 
 def filter_data(data):
@@ -94,8 +120,8 @@ def arima_model(name_data, ts_data, p=None, d=None, q=None, prdct=None):
         nd = {dic[key]['year']: float(dic[key]['value'])
               for key in dic}
 
-        predictions = {year_since + i: float(prediction[i-1])
-                       for i in range(1, len(prediction)+1)}
+        predictions = {year_since + i: float(prediction[i - 1])
+                       for i in range(1, len(prediction) + 1)}
         results = model_fit.summary()
         now = str(datetime.now().timestamp())
         filename = 'models/ARIMA_model_' + '_'.join(name_data.split()) + '_' + now[:now.find('.')] + '.txt'
@@ -112,3 +138,9 @@ def arima_model(name_data, ts_data, p=None, d=None, q=None, prdct=None):
 
     except Exception as error:
         return error, dict(), dict()
+
+
+data = [[randint(1, 50)*randint(-2.5, 0.3)
+         for _ in range(100)]
+        for _ in range(100)]
+print(get_stationary_i(data))
